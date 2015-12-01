7a474cf250b739f1275353482ba3e218eeaeeefc
masteret <kaheichan@utexas.edu>
2015-12-01 12:45:28 -0600

    add multiple words parsing in one line

--- parser.py
+++ parser.py
@@ -1,10 +1,23 @@
 import json
 
 def find_text(text, chi_dict):
+    words = text.split()
+    find_word(words, "", chi_dict)
+
+def find_word(words, append, chi_dict):
+    if len(words) == 0:
+        print(append)
+    else:
+        for word in _find_text(words[0], chi_dict):
+            if word != "Not Found":
+                find_word(words[1:], append+word, chi_dict)
+
+
+def _find_text(text, chi_dict):
     if len(text) == 0:
-        print("Not found")
+        return "Not Found"
     elif text in chi_dict:
-        print(chi_dict[text])
+        return chi_dict[text]
     else:
         find_text(text[:-1], chi_dict)
 
